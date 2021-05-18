7c0a783181a59b8a0e4ddc17f5cda90946926e41
Nik Eugster <mail@nikeugster.ch>
2021-05-18 17:01:11 +0200

    Commit before live code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(
+    title:   Faker::Hacker.adjective.capitalize,
+    content: Faker::Hacker.say_something_smart,
+  )
+  article.save!
+end
